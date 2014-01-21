d92e3dd8cc8484d1dc1d4c0273d3eefa6edc09fe
Jelle Luteijn <jelle.luteijn@gmail.com>
2014-01-21 19:05:17 +0100

    beginnnetje parser

---
+++ DiskUsage/src/diskusage/Disk.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package diskusage;
+
+/**
+ *
+ * @author jelle
+ */
+public class Disk {
+    private String naam;
+    private int percentUsed;
+    
+    public Disk(String nm, int pu) {
+        naam = nm;
+        percentUsed = pu;
+    }
+    
+    public String getNaam() {
+        return naam;
+    }
+    
+    public int getPercentUsed() {
+        return percentUsed;
+    }
+}

f7b7a203a16a514e6a6a756ee5e0f73526af3326
swhiteley91 <simonwhiteley@hotmail.com>
2014-01-22 22:51:20 +0100

    New entities

---
+++ SpaceInvaders/src/spaceinvaders/ShipEntity.java
@@ -0,0 +1,28 @@
+package spaceinvaders;
+
+public class ShipEntity extends Entity {
+	private SpaceInvaders game;
+	
+	public ShipEntity(SpaceInvaders game,String ref,int x,int y) {
+		super(ref,x,y);
+		
+		this.game = game;
+	}
+	
+	public void move(long delta) {
+		if ((dx < 0) && (x < 10)) {
+			return;
+		}
+		if ((dx > 0) && (x > 750)) {
+			return;
+		}
+		
+		super.move(delta);
+	}
+	
+	public void collidedWith(Entity other) {
+		if (other instanceof AlienEntity) {
+			game.notifyDeath();
+		}
+	}
+}

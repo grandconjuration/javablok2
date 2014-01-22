f03e52fa889cc41d5b2a0c30520d9a456a98adfc
Jelle Luteijn <jelle.luteijn@gmail.com>
2014-01-22 15:23:46 +0100

    Assets toegevoegd en begin aan de muziek speler

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 javablok2
 =========
 School opdracht Blok b - Informatica - Hogeschool Utrecht
+
+Music:
+"Space Fighter Loop" Kevin MacLeod (incompetech.com)
+Licensed under Creative Commons: By Attribution 3.0
+http://creativecommons.org/licenses/by/3.0/
---
+++ SpaceInvaders/src/spaceinvaders/Music.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spaceinvaders;
+
+import javafx.scene.media.MediaPlayer;
+
+/**
+ *
+ * @author jelle
+ */
+public class Music {
+    private static MediaPlayer mediaPlayer;
+    
+}

61207a54ea56d12a04944bbcfb70eb1ac1804dc2
swhiteley91 <simonwhiteley@hotmail.com>
2014-01-22 22:48:51 +0100

    Entity, AlienEntity

---
+++ SpaceInvaders/src/spaceinvaders/AlienEntity.java
@@ -0,0 +1,38 @@
+package spaceinvaders;
+
+
+public class AlienEntity extends Entity {
+	private double moveSpeed = 75;
+	private SpaceInvaders game;
+	
+	public AlienEntity(SpaceInvaders game,String ref,int x,int y) {
+		super(ref,x,y);
+		
+		this.game = game;
+		dx = -moveSpeed;
+	}
+
+	public void move(long delta) {
+		if ((dx < 0) && (x < 10)) {
+			game.updateLogic();
+		}
+		if ((dx > 0) && (x > 750)) {
+			game.updateLogic();
+		}
+		
+		super.move(delta);
+	}
+	
+	public void doLogic() {
+		dx = -dx;
+		y += 10;
+		
+		if (y > 570) {
+			game.notifyDeath();
+		}
+	}
+	
+	public void collidedWith(Entity other) {
+		// collisions with aliens are handled elsewhere
+	}
+}
